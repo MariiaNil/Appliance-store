140b762326c6b46a1d8d37bcfff0ce546d0bd2b6
mari2 <mari2oo3.mn@gmail.com>
2025-04-08 02:00:38 +0300

    DONE add redirect http to Https

--- src/main/java/com/epam/rd/autocode/assessment/appliances/dto/ClientDTO.java
+++ src/main/java/com/epam/rd/autocode/assessment/appliances/dto/ClientDTO.java
@@ -2,13 +2,19 @@ package com.epam.rd.autocode.assessment.appliances.dto;
 
 import com.epam.rd.autocode.assessment.appliances.password.ValidPassword;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record ClientDTO(
         Long id,
+        @NotBlank(message = "Name cannot be empty")
+        @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
         String name,
-        @Email
+        @Email(message = "Invalid email format")
         String email,
         @ValidPassword
         String password,
+        @Pattern(regexp = "\\d{4}-\\d{4}", message = "Invalid card format")
         String card
 ) { }
--- src/main/java/com/epam/rd/autocode/assessment/appliances/dto/EmployeeDTO.java
+++ src/main/java/com/epam/rd/autocode/assessment/appliances/dto/EmployeeDTO.java
@@ -1,9 +1,21 @@
 package com.epam.rd.autocode.assessment.appliances.dto;
 
+import com.epam.rd.autocode.assessment.appliances.password.ValidPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record EmployeeDTO(
         Long id,
+        @NotBlank(message = "Name is required")
+        @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
         String name,
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email must be valid")
         String email,
+        @NotBlank(message = "Password is required")
+        @ValidPassword
         String password,
+        @NotBlank(message = "Department is required")
         String department
 ) { }
